aa27dc6da39c96373120828910fc4468453b855d
kyoungjin2310 <kyoungjin2310@gmail.com>
2021-06-02 07:51:55 +0900

    "change"

--- temple/client/src/components/Header.js
+++ temple/client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <Row>
         <Col md="6" sm="auto" className="text-center m-auto">
           <h1>Read Our Blog</h1>
-          <p>사플의 사이드 프로젝트 작업블로그 입니다</p>
+          <p>작업블로그</p>
         </Col>
       </Row>
     </div>
--- temple/client/src/redux/sagas/index.js
+++ temple/client/src/redux/sagas/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 import authSaga from "./authSaga";
 import postSaga from "./postSaga";
-import commentSaga from "./commentSaga";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,5 +10,5 @@ dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
-  yield all([fork(authSaga), fork(postSaga), fork(commentSaga)]);
+  yield all([fork(authSaga), fork(postSaga)]);
 }
